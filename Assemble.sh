1f3e88aeae6ac06084c4335c70d2d11bd943403d
WaffleStudios <80982208+mrwaaafles@users.noreply.github.com>
2023-01-10 21:49:56 -0600

    Update Assemble.sh

--- Assemble.sh
+++ Assemble.sh
@@ -1,8 +1,8 @@
 export PATH=$PATH:/usr/local/i386elfgcc/bin
 
-nasm "boot.asm" -f bin -o "Binaries/boot.bin"
-nasm "kernel_entry.asm" -f elf -o "Binaries/kernel_entry.o"
-i386-elf-gcc -ffreestanding -m32 -g -c "kernel.cpp" -o "Binaries/kernel.o"
+nasm "boot/boot.asm" -f bin -o "Binaries/boot.bin"
+nasm "boot/kernel_entry.asm" -f elf -o "Binaries/kernel_entry.o"
+i386-elf-gcc -ffreestanding -m32 -g -c "kernel/kernel.cpp" -o "Binaries/kernel.o"
 nasm "zeroes.asm" -f bin -o "Binaries/zeroes.bin"
 
 i386-elf-ld -o "Binaries/full_kernel.bin" -Ttext 0x1000 "Binaries/kernel_entry.o" "Binaries/kernel.o" --oformat binary
